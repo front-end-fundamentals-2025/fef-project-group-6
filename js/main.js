c4e2c6133814a8a2e87c33d1371713094be062b0
nivinmuhamed <muni24by@student.ju.se>
2025-03-12 23:45:04 +0100

    finished the localstorage

--- js/main.js
+++ js/main.js
@@ -8,15 +8,13 @@ buttonButton?.addEventListener("click", function () {
 
 trashcanButton?.addEventListener("click", function () {
   localStorage.setItem("product", "false");
+  purchaseElement.style.display = "none";
 });
 
 const paintingsInCart = localStorage.getItem("product");
 
 if (paintingsInCart === "true") {
-  purchaseElement.style.display = "block"; 
+  purchaseElement.style.display = "block";
 } else {
-  purchaseElement.style.display = "none",
+  purchaseElement.style.display = "none";
 }
-
-
-
